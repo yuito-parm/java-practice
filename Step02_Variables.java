b0bc98d180d5f051d0455fa931f9b39cd83cae81
yuito-kijima <pmppmppmppmppmp@yahoo.co.jp>
2025-05-23 19:53:03 +0900

    変数と四則演算の基本

---
+++ Step02_Variables.java
@@ -0,0 +1,16 @@
+public class Step02_Variables {
+    public static void main(String[] args) {
+        int x = 7;
+        int y = 3;
+
+        int sum = x + y;
+        int difference = x - y;
+        int product = x * y;
+        int quotient = x / y;
+
+        System.out.println("足し算 : " + sum);
+        System.out.println("引き算 : " + difference);
+        System.out.println("掛け算 : " + product);
+        System.out.println("割り算 : " + quotient);
+    }
+}

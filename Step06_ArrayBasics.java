d6fbf22009b79023657b57da5e38118a5494355a
yuito-kijima <pmppmppmppmppmp@yahoo.co.jp>
2025-05-23 20:29:00 +0900

    配列＋if：80点以上にメッセージ＋平均点も表示

--- Step06_ArrayBasics.java
+++ Step06_ArrayBasics.java
@@ -2,8 +2,15 @@ public class Step06_ArrayBasics {
     public static void main(String[] args) {
         int[] scores =  {72, 85, 90, 68, 77};
 
+        int total = 0;
+        System.out.println("80点以上の人だけを表示");
         for (int i = 0; i < scores.length; i++) {
-            System.out.println((i + 1) + "人目の点数: " + scores[i] + "点");
+            if (scores[i] >= 80) {
+                System.out.println((i + 1) + "人目の点数: " + scores[i] + "点");
+            }
+            total = total + scores[i];
         }
+        double average = (double)total / scores.length;
+        System.out.println("平均点: " + average + "点");
     }
 }

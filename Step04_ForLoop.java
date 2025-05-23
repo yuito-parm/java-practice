eeb2df7f848a31fada854661bd48f3e9affeb7fa
yuito-kijima <pmppmppmppmppmp@yahoo.co.jp>
2025-05-23 20:08:57 +0900

    for文の基本：5回繰り返すループ

---
+++ Step04_ForLoop.java
@@ -0,0 +1,7 @@
+public class Step04_ForLoop {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(i + "回目のループです！");
+        }
+    }
+}
